24657707f12d0e534a919a1a4fd9477afbaa3d84
praveen8978-ux <praveen.beeram7@gmail.com>
2025-09-22 12:45:57 +0530

    welcome email implemented

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,12 +1,13 @@
 import jwt from "jsonwebtoken";
+import { ENV } from "./env.js";
 
 export const generateToken = (userId, res) => {
-  const { JWT_SECRET, NODE_ENV } = process.env;
+  const { JWT_SECRET } = ENV;
   if (!JWT_SECRET) {
     throw new Error("JWT_SECRET is not configured");
   }
 
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+  const token = jwt.sign({ userId }, JWT_SECRET, {
     expiresIn: "7d",
   });
 
@@ -14,8 +15,11 @@ export const generateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks: cross-site scripting
     sameSite: "strict", // CSRF attacks
-    secure: process.env.NODE_ENV === "development" ? false : true,
+    secure: ENV.NODE_ENV === "development" ? false : true,
   });
 
   return token;
 };
+
+// http://localhost
+// https://dsmakmk.com
